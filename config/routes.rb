3ce2e87b209197adce47c13c17eeb43625cce003
Masa H <masamasa0669@gmail.com>
2022-06-17 23:25:14 +0900

    ルーティングの修正、controllerアクション修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :menu, only: %i[index]
-      resources :ship, only: %i[index]
+      resources :ships, only: %i[index]
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
