30d5a6408ac0c18c8ce1b2ee23626076d88e4b62
Masa H <masamasa0669@gmail.com>
2022-07-11 09:34:05 +0900

    スケジュールのindexアクションを通常のものに変更

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::SchedulesController < ApplicationController
   def index
-    @ship = Ship.find(params[:ship_id])
-    @schedules = @ship.schedules
-    render json: @schedules
+      @schedules = Schedule.all
   end
 end
