762e4e2591cb660b14096e50f716dac2214e634a
Masa H <masamasa0669@gmail.com>
2022-08-17 12:15:08 +0900

    不要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :sections, only: :index do
-        member do
-          get 'roles'
-        end
-      end
       resources :ships, only: :index do
         member do
           get 'maintenances'
