79019718dee9fe5479c1d5471afe8b0d153b3ba2
Masa H <masamasa0669@gmail.com>
2022-10-02 10:10:47 +0900

    CORS間違えた。 react側のURL記載

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001", "http://3.113.185.225"
+    origins "localhost:3001", "https://master.d37zayt9korlrr.amplifyapp.com/"
 
     resource "*",
       headers: :any,
